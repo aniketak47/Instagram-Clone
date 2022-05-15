3c5326882b52cc0ea7c727567aac9e386047b589
aniketak47 <aniketkr20012019@gmail.com>
2022-05-15 23:14:38 +0530

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { auth, } from "../firebase";
+import { signInWithEmailAndPassword,signOut } from "firebase/auth";
+function Login() {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+
+  let [user, setUser] = useState(null);
+  let [loader, setLoader] = useState(false);
+  let [error, setError] = useState("");
+  const trackEmail = function (e) {
+    setEmail(e.target.value);
+  }
+  const trackPassword = function (e) {
+    setPassword(e.target.value);
+  }
+  const printDetails = async function () {
+    // alert(email + " " + password);
+    try {
+      setLoader(true);
+      let userCred = await
+        signInWithEmailAndPassword(auth, email, password)
+      // console.log(userCred.user);
+      setUser(userCred.user);
+    } catch (err) {
+      setError(err.message);
+      // after some time -> error message remove 
+      setTimeout(() => {
+        setError("")
+      }, 2000)
+    }
+    setLoader(false);
+  }
+  const signout = async function () {
+    await signOut(auth);
+    setUser(null);
+  }
+
+  
+  return (
+    <>
+      {
+        error !== "" ? <h1>Error is {error}</h1> :
+          loader === true ? <h1>...Loading</h1> :
+            user != null ?
+              <>
+                <button
+                  onClick={signout}
+                >Signout</button>
+                <h1>user is {user.uid}</h1>
+              </>
+              :
+              <><input type="email" onChange={trackEmail} value={email} placeholder="email" ></input>
+                <br></br>
+                <input type="password" onChange={trackPassword} value={password} placeholder="password"
+                ></input>
+                <br></br>
+                <button type="click" onClick={printDetails}
+                >Login</button></>
+      }
+
+    </>
+  )
+}
+export default Login
